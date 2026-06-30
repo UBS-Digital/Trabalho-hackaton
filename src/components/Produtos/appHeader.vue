b3bd9ce7a029ffe86233e88bf90045da4a26a6c4
AnaPaula-03 <AnaKotchella9@gmail.com>
2026-06-30 09:12:22 -0300

    Criação do home e de alguns codigos no app vue e em outras pastas

--- src/components/Produtos/appHeader.vue
+++ src/components/Produtos/appHeader.vue
@@ -1,10 +1,15 @@
 <script setup>
 
-
+import { RouterLink } from 'vue-router';
 
 </script>
 <template>
 
+    <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/">AgendarConsulta</RouterLink>
+        <RouterLink to="/">AgendarExame</RouterLink>
+        <RouterLink to="/">MedicoFamiliar</RouterLink>
+    </nav>
 
-    
 </template>
