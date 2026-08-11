c121dd730c117f8d49805029d78ed9638dfb0d8f
KevinRubensSantana <kevinrubens9410@gmail.com>
2026-08-11 16:50:27 -0300

    FEAT #14: W voltar

--- src/views/EspecialidadesMedicos.vue
+++ src/views/EspecialidadesMedicos.vue
@@ -1,7 +1,11 @@
 <script setup>
+ import { RouterLink } from 'vue-router';
 </script>
 <template>
     <div class="banner">
+        <div class="voltar">
+            <RouterLink to="/"><- Voltar</RouterLink>
+        </div>
         <h2>
             Especialidades Médicas
         </h2>
@@ -12,17 +16,21 @@
 </template>
 <style scoped>
 .banner {
-    padding: 3vw 2.5vw 3vw 2.5vw;
+    padding: 1VW 0.85VW;
     background: linear-gradient(170deg, #2053ac, #447CDB, #267154, #267154, #37c22f);
     color: whitesmoke;
+}
+.banner a {
+    color: white;
 }
 h2 {
     font-size: 1.67rem;
     font-weight: bolder;
-    margin-bottom: 1.3vw;
+    margin: 3vw 1vw 0 2.5vw;
 }
 p {
     font-size: 1.1rem;
+    margin: 1vw 2.5vw 3vw 2.5vw;
 }
 
 </style>
