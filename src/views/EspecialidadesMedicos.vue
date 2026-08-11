cb56987a3490a6d5e3baf1f43b32ea4f9f88a91e
KevinRubensSantana <kevinrubens9410@gmail.com>
2026-08-11 16:37:04 -0300

    FEAT #14: W banner

--- src/views/EspecialidadesMedicos.vue
+++ src/views/EspecialidadesMedicos.vue
@@ -1,7 +1,28 @@
 <script setup>
 </script>
 <template>
-b
+    <div class="banner">
+        <h2>
+            Especialidades Médicas
+        </h2>
+        <p>
+            Conheça todas as especialidades disponíveis na UBS Digital e agende sua consulta
+        </p>
+    </div>
 </template>
 <style scoped>
+.banner {
+    padding: 3vw 2.5vw 3vw 2.5vw;
+    background: linear-gradient(170deg, #2053ac, #447CDB, #267154, #267154, #37c22f);
+    color: whitesmoke;
+}
+h2 {
+    font-size: 1.67rem;
+    font-weight: bolder;
+    margin-bottom: 1.3vw;
+}
+p {
+    font-size: 1.1rem;
+}
+
 </style>
