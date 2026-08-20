6c6322775203a9e58a1aae8c28c738c53b89e524
Lucas-ElielL <lucaseliel15032010@gmail.com>
2026-08-20 09:30:47 -0300

    FEAT #20: Continuação do menu haburger

--- src/components/Produtos/AppHeader.vue
+++ src/components/Produtos/AppHeader.vue
@@ -114,12 +114,5 @@ nav {
   .mudar-menu {
     display: block;
   }
-  #menu ul {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-    padding: 0;
-  }
 }
 </style>
