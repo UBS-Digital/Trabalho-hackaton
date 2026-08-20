1edce18ba470e54202aaf0189ba9efe34777d008
pedrobanfi <pedrogabrielbanfi@gmail.com>
2026-08-20 09:31:39 -0300

    bla

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -71,6 +71,62 @@ import { RouterLink,  } from 'vue-router'
 
 </div>
 
+
+
+<div class="corpo">
+
+    <h2>Nossos serviços</h2>
+    <p>
+        Atendimento completo com especialidades médicas e exames para cuidar da sua 
+        saúde.
+    </p>
+</div>
+    
+<div class="servicos">
+
+  <div class="clinico">
+    <div class="cabecalhoUm">
+      <img src="/src/imagem/clinicoGeral.png" alt="">
+      <h2>Clínico Gerall</h2>
+    </div>
+
+    <p>Atendimento completo para diagnóstico e tratamento de doenças comuns</p>
+  </div>
+
+
+  <div class="pediatra">
+    <div class="cabecalhoDois">
+      <img src="/src/imagem/pediatra.png" alt="">
+      <h2>Pediatria</h2>
+    </div>
+
+    <p>Cuidado especializado para a saúde e desenvolvimento das crianças</p>
+  </div>
+
+
+  <div class="ginecologista">
+    <div class="cabecalhoTres">
+      <img src="/src/imagem/ginecologista.png" alt="">
+      <h2>Ginecologia</h2>
+    </div>
+
+    <p>Saúde da mulher com atendimento humanizado e preventivo</p>
+  </div>
+
+
+  <div class="medicoFamilia">
+    <div class="cabecalhoQuatro">
+      <img src="/src/imagem/medicoFamilia.png" alt="">
+      <h2>Médico da Família</h2>
+    </div>
+
+    <p>Acompanhamento contínuo e integral de toda sua família </p>
+  </div>
+
+</div>
+
+
+
 </template>
 <style scoped>
 
@@ -213,9 +269,237 @@ img{
     line-height: 1.4;
     color: #999;
 }
+.corpo{
+    background-color: #f5f5f5;
 
+}
+.corpo h2{
+    text-align: center;
+    font-weight: bold;
+    font-size: 7rem;
+    padding: 5rem 5rem 1.5rem 5rem
 
+}
+.corpo p{
+    text-align: center;
+    font-size: 2rem;
+    color: #918686;
+    
+}
 
+.servicos{
+    background-color: #f5f5f5;
+    justify-content: space-between;
+    display: flex;
+    margin: 0 0 3rem 0;
+}
+.servicos .clinico{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoUm{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.clinico p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
+
+.servicos{
+    background-color: #f5f5f5;
+    justify-content: space-between;
+    display: flex;
+    margin: 0 0 3rem 0;
+}
+.servicos .clinico{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoUm{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.clinico p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
+
+.servicos .pediatra{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoDois{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.pediatra p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
+
+.servicos{
+    background-color: #f5f5f5;
+    justify-content: space-between;
+    display: flex;
+    margin: 0 0 3rem 0;
+}
+.servicos .pediatra{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoDois{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.pediatra p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
+
+.servicos .ginecologista{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoTres{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.ginecologista p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
+
+.servicos{
+    background-color: #f5f5f5;
+    justify-content: space-between;
+    display: flex;
+    margin: 0 0 3rem 0;
+}
+.servicos .ginecologista{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoTres{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.ginecologista p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
+Aqui
+.servicos .medicoFamilia{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoQuatro{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.medicoFamilia p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
+
+.servicos{
+    background-color: #f5f5f5;
+    justify-content: space-between;
+    display: flex;
+    margin: 0 0 3rem 0;
+}
+.servicos .medicoFamilia{
+    background-color: white;
+    border-radius: 4px;
+    width: 418px;
+    height: 432px;
+    margin: 25px 0 0 5rem;
+}
+.cabecalhoQuatro{
+ display: flex;
+    align-items: center;
+    gap: 10px;
+    padding: 17px;
+h2{
+    font-size: 1.8rem;
+}
+}
+.medicoFamilia p{
+    color: #696666;
+    font-size: 1.5rem;
+    padding: 0 17px;
+     line-height: 1.5;
+}
 
 
 </style>
