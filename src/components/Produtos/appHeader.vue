fcce265d241a2fe05ca29ffc2652dcdeb845eece
gamaliel6271 <gama6969g@gmail.com>
2026-08-27 09:35:52 -0300

    FEAT:finalizaçao do headerrsrs

--- src/components/Produtos/appHeader.vue
+++ src/components/Produtos/appHeader.vue
@@ -25,8 +25,8 @@ import { RouterLink } from 'vue-router';
       <RouterLink to="/especialidades">Especialidades</RouterLink>
       <RouterLink to="/contato-site">Contato</RouterLink>
      </div>
-
-
+ 
+ 
 
         <div class="botoes">
           <RouterLink to="/MinhaArea" class="btn"> <img src="/src/imagem//icon-minha-area.svg" alt="" class="imgiconzinho">Minha Área</RouterLink>
@@ -45,10 +45,10 @@ import { RouterLink } from 'vue-router';
 
 
 header {
-  display: flex;
+font-family: "Nunito Sans", sans-serif;  display: flex;
   align-items: center;
   justify-content: center;
-  padding: 2vw;
+  padding: 1vw;
   color: #5281e6;
 
 }
@@ -69,6 +69,7 @@ h1{
   
   color: black;
   display: flex;
+  padding-right: 2vw;
 }
 
 
@@ -81,32 +82,25 @@ margin-top: 0.7vw;
 
 .mini {
   font-size: 12px;
-}
-
-a{
   color: #5f6061;
 }
 
-.btn {
-  border: 0.3px solid #5281e6;
-  padding: 0.2vw 0.7vw;
-  margin: 5px;
-  border-radius: 0.6vw;
-  align-items: center;
-  display: flex;
-  gap: 0.3vw;
-  color: #5281e6;
-  
+a{
+  color: #828385;
+  font-size: 15px;
 }
+
 
 
 nav {
   display: flex;
   align-items: center;
+  
 }
 .botoes {
   display: flex;
-
+  padding-left: 1vw;
+  
 }
 .atual {
   color: rgb(4, 132, 51);
@@ -119,16 +113,69 @@ nav {
 }
 
 
+.listas {
+  
+  display: flex;
+  gap:  0.7vw;
+
+}
+.btn {
+  position: relative;
+  overflow: hidden;
+  border: 0.3px solid #5281e6;
+  padding: 0.35vw 1.2vw;
+  margin: 7px;
+  border-radius: 0.4vw;
+  align-items: center;
+  display: flex;
+  gap: 0.3vw;
+  color: #5281e6;
+  font-size: 1vw;
+  transition: color 0.3s ease;
+  z-index: 1;
+}
+
+.btn::before {
+  content: "";
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  height: 0;
+  background-color: #5281e6;
+  transition: height 0.3s ease;
+  z-index: -1;
+}
+
+.btn:hover::before {
+  height: 100%;
+}
+
+.btn:hover {
+  color: white;
+}
+
+
+
 .btn-2 {
-    border: 0.5px solid #5281e6;
-  padding: 0.2vw 0.7vw;
-  margin: 5px;
-  border-radius: 0.6vw;
+  border: 0.5px solid #5281e6;
+  padding: 0.35vw 1.2vw;
+  margin: 6px;
+  border-radius: 0.4vw;
   align-items: center;
   display: flex;
   gap: 0.3vw;
   background-color: #3a70e2;
   color: white;
-  
+  font-size: 1vw;
+  transition: 0.2s ease;
 }
+
+.btn-2:hover {
+  background-color: #285fc9;
+  transform: scale(0.95);
+}
+
+
+
 </style>
