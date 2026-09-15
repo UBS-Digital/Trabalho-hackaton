a24f24c45de0a6e9220aab2f56327df2f7d6b363
AnaPaula-03 <anakotchella9@gmail.com>
2026-09-15 09:29:45 -0300

    .

--- src/views/hemogramaCompletoView.vue
+++ src/views/hemogramaCompletoView.vue
@@ -60,17 +60,18 @@ const localAtendimento = ref('')
 
     </div>
     <div class="colunas">
-        <div class="coluna">
+        <div class="coluna1">
             <div class="imagem">
                 <h2>Selecionar o Local</h2>
                 <img src="/public/iconeLocal.png" alt="">
-            </div>
-            <select v-model="localAtendimento">
+            </div>    
+                <select v-model="localAtendimento">
                 <option value="">UBS digital - Unidade Norte</option>
                 <option value="ubs">UBS digital - Laboratório central</option>
             </select>
+            
         </div>
-        <div class="coluna">
+        <div class="coluna2">
             <div class="data">
                 <h2>
                     Selecione Data e Horário
@@ -85,10 +86,10 @@ const localAtendimento = ref('')
 
     <div class="botoes">
 
-        <button>← Voltar</button>
+        <button><RouterLink to="/agendar-exame" class="btn primary"><span><- voltar</span> </RouterLink></button>
 
-        <button @click="proximo">Próximo →</button>
-
+        <button><RouterLink to="/agendar-consulta" class="btn primary"><span> Confirmar -></span> </RouterLink>
+</button> 
     </div>
 
 </template>
@@ -100,11 +101,34 @@ const localAtendimento = ref('')
     gap: 20px;
 }
 
-.coluna {
+.coluna1, .coluna2 {
     flex: 1;
     display: flex;
     flex-direction: column;
     align-items: center;
+     transform: scale(1.3);
+}
+
+.coluna1 {
+    background-color: white;
+    box-shadow: 0px 10px 30px rgba(0, 0, 0, 0.2);
+    max-width: 400px;
+    border-radius: 20px;
+    margin-top: 5.65rem;
+    margin-bottom: 5.65rem;
+    margin-left: 17rem;    
+}
+.imagem{
+    margin: 9vw 0 2vw 0;
+    transform: scale(1.7);
+    background: linear-gradient(to right, #4DDC45, #267154, #6949A8);
+    border-radius: 2px;
+    padding: 4px;
+    }
+.data{
+    margin: 5vw;
+     background: linear-gradient(to right, #6949A8, #267154, #4DDC45);
+      border-radius: 9px;
 }
 .imagem {
     display: flex;
@@ -114,9 +138,7 @@ const localAtendimento = ref('')
     width: 20px;
     height: auto;
 }
-.imagem h2{
-    
-}
+
 div.Banner {
     display: flex;
     justify-content: space-between;
@@ -203,7 +225,7 @@ div.Banner {
     font-size: 2rem;
 }
 
-.local select {}
+
 
 .linha :hover {
     width: 100%;
@@ -231,16 +253,17 @@ button {
     border: none;
     border-radius: 8px;
     background: #2f80ed;
-    color: white;
     cursor: pointer;
 
 }
+
+span {
+    color: white;
+}
+
 .data{
     display: flex;
     flex-direction: column;
     align-items: center;
-}
-.calendario {
-
 }
 </style>
