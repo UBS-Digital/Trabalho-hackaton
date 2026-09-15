566874a25c4394d71c3eaa138ac0211e76c6984b
Lucas-ElielL <lucaseliel15032010@gmail.com>
2026-09-15 08:27:37 -0300

    FEAT #55: CSS da página de cadastro

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,28 +1,75 @@
 <script setup>
 </script>
 <template>
-<section class="login">
-  <div>
-    <h2>Cadastre-se:</h2>
-    <form action="login">
-      <label for="nome">Nome Completo:</label>
-      <input type="text" id="nome" placeholder="Nome Completo*" required>
-      <label for="cpf">CPF:</label>
-      <input type="text" id="cpf" placeholder="CPF*">
-      <label for="telefone">Telefone:</label>
-      <input type="number" id="telefone" placeholder="Número de Telefone*">
-      <p>Gênero</p>
-      <label for="genero">M</label>
-      <input type="radio" id="genero" name="genero" value="1">
-      <label for="genero">F</label>
-      <input type="radio" name="genero" id="genero" value="2">
-      <label for="email">E-mail</label>
-      <input type="email" name="email" id="email" required placeholder="Email*">
-      <label for="">Crar Senha</label>
-      <input type="password" name="senha" id="senha" required placeholder="Criar Senha*">
-    </form>
-  </div>
-</section>
+  <section class="cadastro">
+    <div class="formulario">
+      <h2>Cadastre-se:</h2>
+      <form action="cadastroUs">
+        <label for="nome">Nome Completo:</label>
+        <input type="text" id="nome" placeholder="Nome Completo*" required>
+        <label for="cpf">CPF:</label>
+        <input type="text" id="cpf" placeholder="CPF*">
+        <label for="telefone">Telefone:</label>
+        <input type="number" id="telefone" placeholder="Número de Telefone*">
+        <div class="gen">
+          <p>Gênero:</p>
+          <label for="genero">M</label>
+          <input type="radio" id="genero" name="genero" value="1">
+          <label for="genero">F</label>
+          <input type="radio" name="genero" id="genero" value="2">
+        </div>
+        <label for="email">E-mail</label>
+        <input type="email" name="email" id="email" required placeholder="Email*">
+        <label for="">Crar Senha</label>
+        <input type="password" name="senha" id="senha" required placeholder="Criar Senha*">
+      </form>
+      <button class="submeter">Cadastrar</button>
+    </div>
+  </section>
 </template>
+
 <style scoped>
+.cadastro {
+  background-color: #F4F3F3;
+  padding: 2vw 15%;
+}
+
+.formulario {
+  background-color: white;
+  box-shadow: 3px 3px 3px 2px rgba(0, 0, 0, 25%);
+  padding: 2vw;
+}
+
+.formulario h2 {
+  font-size: 1.8rem;
+  margin: 0 0 20px 0;
+}
+
+form {
+  display: flex;
+  flex-direction: column;
+}
+form label {
+  font-weight: bold;
+  font-size: 1rem;
+  margin: 10px 0 0 0;
+}
+form input {
+  padding: 10px 10px;
+  border-radius: 10px;
+  border: 1px solid rgb(43, 43, 43);
+}
+.submeter {
+  background-color: #4D41EF;
+  color: white;
+  padding: 0.5vw 2.5vw 0.5vw 3vw;
+  border-radius: 10px;
+  border: none;
+  font-size: 1rem;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 2vw 0 0 0;
+}
 </style>
