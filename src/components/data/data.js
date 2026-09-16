3138fe76540261eac6c154b79a1213d4614d328d
Gian-Carlos-Urbano <giancarlosurbano18@gmail.com>
2026-09-16 17:49:30 -0300

    corrigido

--- src/components/data/data.js
+++ src/components/data/data.js
@@ -194,6 +194,16 @@ const medicosBase = ref([
   }
 ])
 
+export const horarios = ref([
+  { id: 1, dia: 'Segunda-feira', horariosDisponiveis: ['08:00 - 20:00'] },
+  { id: 2, dia: 'Terça-feira', horariosDisponiveis: ['08:00 - 20:00'] },
+  { id: 3, dia: 'Quarta-feira', horariosDisponiveis: ['08:00 - 20:00'] },
+  { id: 4, dia: 'Quinta-feira', horariosDisponiveis: ['08:00 - 20:00'] },
+  { id: 5, dia: 'Sexta-feira', horariosDisponiveis: ['08:00 - 20:00'] },
+  { id: 6, dia: 'Sabado', horariosDisponiveis: ['08:00 - 17:00'] },
+  { id: 7, dia: 'Domingo', horariosDisponiveis: ['FECHADO'] }
+]);
+
 export default paginas;
 export { pacientesBase };
 export { medicosBase }
